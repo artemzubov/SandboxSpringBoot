4166871f54ce53d08419600069e922067f7c1830
Artem Zubov <artem@zubov.spb.ru>
2019-02-25 17:31:40 +0300

    Add DB and add filters for data.

--- src/main/java/ru/spb/zubov/GreetingController.java
+++ src/main/java/ru/spb/zubov/GreetingController.java
@@ -1,13 +1,21 @@
 package ru.spb.zubov;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.spb.zubov.domain.Message;
+import ru.spb.zubov.repos.MessageRepo;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
@@ -17,8 +25,41 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("someText", "someText");
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
         return "main";
     }
 
+    @PostMapping
+    public String add(@RequestParam String text,
+                      @RequestParam String tag,
+                      Map<String, Object> model){
+
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String add(@RequestParam String filter,
+                      Map<String, Object> model){
+
+        Iterable<Message> byTag;
+        if (filter == null || filter.isEmpty()){
+            byTag = messageRepo.findAll();
+        }
+        else {
+            byTag = messageRepo.findByTag(filter);
+        }
+
+        model.put("messages", byTag);
+
+        return "main";
+    }
 }
---
+++ src/main/java/ru/spb/zubov/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package ru.spb.zubov.repos;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.spb.zubov.domain.Message;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+
+}
