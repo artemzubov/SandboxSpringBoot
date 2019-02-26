199841e6a4e914238d970ef562efc5069576f1a4
Artem Zubov <artem@zubov.spb.ru>
2019-02-26 13:01:03 +0300

    Add Security

---
+++ src/main/java/ru/spb/zubov/domain/Role.java
@@ -0,0 +1,5 @@
+package ru.spb.zubov.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/ru/spb/zubov/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package ru.spb.zubov.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.spb.zubov.domain.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+}
