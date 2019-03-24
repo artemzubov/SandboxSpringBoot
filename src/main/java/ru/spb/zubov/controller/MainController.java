2725645c0169e3cc4be0958751d3b59f45ada40a
Artem Zubov <artem@zubov.spb.ru>
2019-03-24 12:31:17 +0300

    Add pictures to messages

--- src/main/java/ru/spb/zubov/controller/MainController.java
+++ src/main/java/ru/spb/zubov/controller/MainController.java
@@ -1,21 +1,30 @@
 package ru.spb.zubov.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.spb.zubov.domain.Message;
 import ru.spb.zubov.domain.User;
 import ru.spb.zubov.repos.MessageRepo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting() {
@@ -45,9 +54,26 @@ public class MainController {
     public String add(@AuthenticationPrincipal User user,
                       @RequestParam String text,
                       @RequestParam String tag,
-                      Model model){
+                      Model model,
+                      @RequestParam("file") MultipartFile file) throws IOException {
 
         Message message = new Message(text, tag, user);
+
+        if (file != null) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+
+            message.setFilename(resultFileName);
+        }
+
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
--- src/main/resources/templates/main.ftl
+++ src/main/resources/templates/main.ftl
@@ -5,9 +5,10 @@
 <body>
 
     <div>
-        <form method="post">
+        <form method="post" enctype="multipart/form-data">
             <input type="text" name="text" placeholder="Insert text">
             <input type="text" name="tag" placeholder="Insert tag">
+            <input type="file" name="file">
             <input type="hidden" name="_csrf" value="${_csrf.token}">
             <button type="submit">Send</button>
         </form>
@@ -21,7 +22,7 @@
     </div>
 
     <form method="get" action="main">
-        <input type="text" name="filter" value="${filter}">
+        <input type="text" name="filter" value="${filter?ifExists}">
         <button type="submit">Filter</button>
     </form>
 
@@ -32,6 +33,11 @@
             <span>${message.text}</span>
             <i>${message.tag}</i>
             <strong>${message.authorName}</strong>
+            <div>
+                <#if message.filename??>
+                    <img src="/img/${message.filename}">
+                </#if>
+            </div>
         </div>
     <#else>
         No messages
